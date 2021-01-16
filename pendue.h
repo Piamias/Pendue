db2f5942fa567777f8b6dc3a2421096c7db79a28
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-16 14:40:17 +0100

    Add random

--- pendue.h
+++ pendue.h
@@ -3,5 +3,9 @@
 #include <string.h>
 
 char readC();
-int win(int* letterfound);
+int win(int* letterfound,int wordsize);
 int searchletter(char letter, char word[], int letterfound[]);
+int pickword(char *word);
+int randompick(int numbermax);
+
+
